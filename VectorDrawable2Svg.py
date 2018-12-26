9419045b9b9791511703b3a96a159216ef29c5df
Rohan Talip <RohanTalip@users.noreply.github.com>
2018-12-25 16:55:24 -0800

    Added a quick hack to substituting values from a color.xml file, allowing for double-indirection.
    Used argparse to parse arguments, with --color-xml-file and --viewbox-only arguments.
    
    Added a color_map dictionary; also added associated read_color_xml() and get_color() functions that write into and read from it.
    
    This is a hack because color_map should probably not be a module-level variable and should instead either be passed around explicitly or it and the related functions should be moved into a new class.
    
    Added a print statement so that it is possible to know what files are being converted, particularly if using something like *.xml on the command line.
    
    Caught any exceptions when converting individual files and printed the stack traceback.

--- VectorDrawable2Svg.py
+++ VectorDrawable2Svg.py
@@ -1,13 +1,49 @@
 """
 VectorDrawable2Svg
-This script convert your VectorDrawable to a Svg
-Author: Alessandro Lucchet
-
-Usage: drop one or more vector drawable onto this script to convert them to svg format
+This script converts VectorDrawable .xml files to SVG files.
+Initial author: Alessandro Lucchet
+Modified by: Rohan Talip
 """
 
-from xml.dom.minidom import *
+import argparse
+from xml.dom.minidom import Document, parse
 import sys
+import traceback
+
+color_map = {}
+
+
+def read_color_xml(file_path):
+    color_xml = parse(file_path)
+    resource_node = color_xml.getElementsByTagName('resources')[0]
+    for color_node in resource_node.getElementsByTagName('color'):
+        name = color_node.attributes['name'].value
+        value = color_node.firstChild.nodeValue
+        if name in color_map:
+            raise 'Color ' + name + ' already exists: ' + color_map[name]
+        color_map[name] = value
+
+
+def get_color(value, depth=1):
+    prefix = '@color/'
+
+    if value.startswith('#'):
+        return value
+
+    if depth >= 3:
+        raise 'Depth is >= 3'
+
+    if prefix not in value:
+        raise '@color not found in ' + value
+
+    name = value.split(prefix)[1]
+    color = color_map.get(name)
+    if not color:
+        return value
+
+    if prefix in color:
+        return get_color(color, depth + 1)
+    return color
 
 
 # extracts all paths inside vd_container and add them into svg_container
@@ -19,11 +55,13 @@ def convert_paths(vd_container, svg_container, svg_xml):
             svg_path = svg_xml.createElement('path')
             svg_path.attributes['d'] = vd_path.attributes[
                 'android:pathData'].value
+
             if vd_path.hasAttribute('android:fillColor'):
-                svg_path.attributes['fill'] = vd_path.attributes[
-                    'android:fillColor'].value
+                svg_path.attributes['fill'] = get_color(
+                    vd_path.attributes['android:fillColor'].value)
             else:
                 svg_path.attributes['fill'] = 'none'
+
             if vd_path.hasAttribute('android:strokeLineJoin'):
                 svg_path.attributes['stroke-linejoin'] = vd_path.attributes[
                     'android:strokeLineJoin'].value
@@ -37,13 +75,14 @@ def convert_paths(vd_container, svg_container, svg_xml):
                 svg_path.attributes['stroke-width'] = vd_path.attributes[
                     'android:strokeWidth'].value
             if vd_path.hasAttribute('android:strokeColor'):
-                svg_path.attributes['stroke'] = vd_path.attributes[
-                    'android:strokeColor'].value
+                svg_path.attributes['stroke'] = get_color(
+                    vd_path.attributes['android:strokeColor'].value)
+
             svg_container.appendChild(svg_path)
 
 
 # define the function which converts a vector drawable to a svg
-def convert_vector_drawable(vd_file_path):
+def convert_vector_drawable(vd_file_path, viewbox_only):
 
     # create svg xml
     svg_xml = Document()
@@ -56,10 +95,12 @@ def convert_vector_drawable(vd_file_path):
 
     # setup basic svg info
     svg_node.attributes['xmlns'] = 'http://www.w3.org/2000/svg'
-    svg_node.attributes['width'] = vd_node.attributes[
-        'android:viewportWidth'].value
-    svg_node.attributes['height'] = vd_node.attributes[
-        'android:viewportHeight'].value
+    if not viewbox_only:
+        svg_node.attributes['width'] = vd_node.attributes[
+            'android:viewportWidth'].value
+        svg_node.attributes['height'] = vd_node.attributes[
+            'android:viewportHeight'].value
+
     svg_node.attributes['viewBox'] = '0 0 {} {}'.format(
         vd_node.attributes['android:viewportWidth'].value,
         vd_node.attributes['android:viewportHeight'].value)
@@ -87,16 +128,39 @@ def convert_vector_drawable(vd_file_path):
     convert_paths(vd_node, svg_node, svg_xml)
 
     # write xml to file
+    svg_file_path = vd_file_path.replace('.xml', '.svg')
     svg_xml.writexml(
-        open(vd_file_path + '.svg', 'w'), indent="", addindent="  ", newl='\n')
+        open(svg_file_path, 'w'), indent="", addindent="  ", newl='\n')
 
 
-# script begin
-if len(sys.argv) > 1:
-    iter_args = iter(sys.argv)
-    next(iter_args)  #skip the first entry (it's the name of the script)
-    for arg in iter_args:
-        convert_vector_drawable(arg)
-else:
-    print("You have to pass me something")
-    sys.exit()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert VectorDrawable .xml files to .svg files.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="e.g. %(prog)s *.xml")
+
+    parser.add_argument(
+        "--color-xml-file", action="append", help="A color.xml file")
+    parser.add_argument(
+        "--viewbox-only",
+        "--viewBox-only",
+        help="Only add the viewBox attribute and not width or height",
+        action="store_true")
+    parser.add_argument("xml_files", nargs="+", metavar='xml-file')
+    args = parser.parse_args()
+
+    if args.color_xml_file:
+        for color_xml_file in args.color_xml_file:
+            read_color_xml(color_xml_file)
+
+    for xml_file in args.xml_files:
+        print("Converting", xml_file)
+        try:
+            convert_vector_drawable(xml_file, args.viewbox_only)
+        except Exception:
+            print("Failed to convert", xml_file)
+            traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
